111125f5d94793f4ab55fae8f097c7972926ebf4
ganesh570 <102854704+ganesh570@users.noreply.github.com>
2025-01-27 00:38:47 +0530

    commitf2

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ const express = require("express");
 const mainRouter=require("./routes/index");
 const cors = require('cors');
 const app=express();
-app.use(cors());
 
-// app.use(cors({
-//     origin: ["*"],
-//     methods: ["POST", "GET"],
-//     credentials: true
-//   }));  
+
+app.use(cors({
+    origin: ["*"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }));  
 app.use(express.json());
 app.use('/api/v1',mainRouter);
 app.listen(3000);
